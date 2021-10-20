4c239e8efa60ab8f34441f8a8aaee9aaf07b4154
Askar312 <askar.kg.com@mail.ru>
2021-10-20 19:37:23 +0600

    feature/ricket2_add_connect_postgresql_with_psycopg2

--- adsiter/settings.py
+++ adsiter/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'adsiter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'adsiter',
+        'USER': 'davide',
+        'PASSWORD': 'jw8s0F4',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
