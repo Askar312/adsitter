ea1fb58cfaeec213bd91e27908fbc6a033a51e0b
9pedobear <kayratsagynbekov@gmail.com>
2021-10-20 19:37:11 +0600

    feature/ticket2_add_connect_postgresql_with_psycopg2

--- adsiter/settings.py
+++ adsiter/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'adsiter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'adsiter',
+        'USER': 'adsiter_user',
+        'PASSWORD': 'adsiter_pass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
