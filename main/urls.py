e8c003598dae3c5b9acb1e1be5182cc266be3a25
9pedobear <kayratsagynbekov@gmail.com>
2021-10-26 15:44:01 +0600

    bugfix/ticket1_fixed_templates

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import index, PostListView, PostCreateView, PostDetailView
+from .views import *
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('clients/', clients, name='clients'),
+    path('contact/', contact, name='contact'),
+    path('browse_ads/', browse_ads, name='browse_ads'),
     path('api/v1/post-list/', PostListView.as_view()),
     path('api/v1/post-create/', PostCreateView.as_view()),
     path('api/v1/post-detail/<int:pk>/', PostDetailView.as_view())
--- main/views.py
+++ main/views.py
@@ -3,11 +3,37 @@ from .serializers import PostSerializers
 from rest_framework import generics
 from .models import Post
 # Create your views here.
+
 def index(request):
     return render(
         request,
         template_name='main/index.html'
     )
+
+def about(request):
+    return render(
+        request,
+        template_name='main/about.html'
+    )
+
+def browse_ads(request):
+    return render(
+        request,
+        template_name='main/browse_ads.html'
+    )
+
+def clients(request):
+    return render(
+        request,
+        template_name='main/clients.html'
+    )
+
+def contact(request):
+    return render(
+        request,
+        template_name='main/contact.html'
+    )
+
 
 class PostListView(generics.ListAPIView):
     serializer_class = PostSerializers
