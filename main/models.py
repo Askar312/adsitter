d7e59e7729d689b48814e252cc7c3b44ee49e88e
9pedobear <kayratsagynbekov@gmail.com>
2021-10-26 16:17:45 +0600

    bugfix/ticket3_add_models_to_project

--- main/models.py
+++ main/models.py
@@ -1,7 +1,82 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+"""
+-----------------
+Contacts
+
+Name
+Email
+Phone
+Message
+-----------------
+-----------------
+Post
+
+Title
+Image
+Likes
+Views
+Created
+Updated
+Draft
+Slug
+User FK
+Category FK
+-----------------
+_________________
+Subcribe email -> Form (need)
+
+Email
+-----------------
+-----------------
+Category -> Form (need)
+
+Title
+Slug
+-----------------
+"""
+
+class Contacts(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Имя заявителя'
+    )
+    email = models.EmailField(
+        verbose_name='Email'
+    )
+    phone = models.CharField(
+        max_length=15,
+        verbose_name='Номер телефона'
+    )
+    message = models.TextField(
+        verbose_name='Сообщение'
+    )
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('contacts', kwargs={'pk':self.pk})
+
+
 class Post(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Пользователь'
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.DO_NOTHING,
+        verbose_name='Категория'
+    )
     title = models.CharField(
         max_length=250,
         verbose_name='Название поста',
@@ -12,10 +87,12 @@ class Post(models.Model):
         verbose_name='Фото'
     )
     likes = models.PositiveIntegerField(
-        verbose_name='Лайки '
+        verbose_name='Лайки',
+        default=0
     )
     views = models.PositiveIntegerField(
-        verbose_name='Просмотры'
+        verbose_name='Просмотры',
+        default=0
     )
     create_at = models.DateTimeField(
         auto_now_add=True,
@@ -29,6 +106,11 @@ class Post(models.Model):
         default=False,
         verbose_name='Черновик'
     )
+    slug = models.SlugField(
+        max_length=255,
+        verbose_name='Url',
+        unique=True
+    )
 
     class Meta:
         verbose_name = 'Пост'
@@ -37,3 +119,60 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug':self.slug})
+
+
+
+class Subscribe(models.Model):
+    email = models.EmailField(
+        verbose_name='Email'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписчики'
+        ordering = ['email']
+
+    def __str__(self):
+        return f"{self.email}"
+
+    def get_absolute_url(self):
+        return reverse('subscribe', kwargs={'pk':self.pk})
+
+
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Заголовок'
+    )
+    slug = models.SlugField(
+        max_length=255,
+        verbose_name='Url',
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug':self.slug})
+
+
+
+
+
+
+
+
+
+
+
