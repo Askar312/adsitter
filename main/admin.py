bed3da497b39e30edba806eb1b3f870798c1148e
Askar Turdaliev <89399948+Askar312@users.noreply.github.com>
2021-11-01 19:52:24 +0600

    Update admin.py
    Admin

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,48 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 
 
-admin.site.register(Contacts)
-admin.site.register(Post)
-admin.site.register(Subscribe)
-admin.site.register(Category)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'email',
+                    'phone',
+                    'message',
+                    )
+
+    list_filter = ('name',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user',
+                    'category',
+                    'title',
+                    'image',
+                    'likes',
+                    'views',
+                    'create_at',
+                    'update_at',
+                    'draft',
+                    'slug',
+
+                    )
+
+    list_filter = ('user',
+                   'title',)
+
+
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',
+                    'slug',)
+
+
+admin.site.register(Contacts, ContactsAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Subscribe, SubscribeAdmin)
+admin.site.register(Category, CategoryAdmin)
