0933f8d30843d2eaed6a21abfd8f5f09790a751d
9pedobear <kayratsagynbekov@gmail.com>
2021-11-03 16:45:26 +0600

    bugfix

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='home'),
@@ -19,4 +21,4 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LoginView.as_view(), name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
